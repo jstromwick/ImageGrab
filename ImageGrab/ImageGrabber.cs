c77373394220754ceecd55ac79a7c4c927fc1134
Jesse Stromwick <jstromwick@gmail.com>
2016-06-09 11:19:56 -0700

    Updating Grabber and Tests to find img tags and get src attribute values. Begining to implement download.

--- ImageGrab/ImageGrabber.cs
+++ ImageGrab/ImageGrabber.cs
@@ -1,14 +1,88 @@
-﻿namespace ImageGrab
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AngleSharp.Parser.Html;
+using RestSharp;
+
+namespace ImageGrab
 {
     public class ImageGrabber
     {
-        public void GetImages(string url, string filePath)
+        public bool GetImages(string url, string filePath)
         {
+            if (!CheckUrl(url))
+            {
+                return false;
+            }
+
+            var html = GetHtml(url);
+
+            if (html == null)
+            {
+                return false;
+            }
+
+            var imageUrls = GetImageUrlsFromHtml(html);
+            DownloadImages(url, imageUrls, filePath);
+
+            return true;
+        }
+
+        internal void DownloadImages(string baseUrl, IEnumerable<string> urls, string filePath)
+        {
+            CreatePath(filePath);
+
+            var uri = new Uri(baseUrl, UriKind.RelativeOrAbsolute);
+            var host = uri.Host;
+
+            foreach (var url in urls)
+            {
+                var downloadUrl = url;
+                if (!url.StartsWith("http"))
+                {
+                    if (url.StartsWith("/"))
+                    {
+                    }
+                }
+            }
+        }
+
+        internal static bool CheckUrl(string url)
+        {
+            return Uri.IsWellFormedUriString(url, UriKind.Absolute);
         }
 
         internal string GetHtml(string url)
         {
-            return null;
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request);
+
+            if (!response.ContentType.StartsWith("text/html"))
+            {
+                return null;
+            }
+
+            return response.Content;
+        }
+
+        internal static IEnumerable<string> GetImageUrlsFromHtml(string html)
+        {
+            var parser = new HtmlParser();
+            var htmlDocument = parser.Parse(html);
+            var imageUrls = htmlDocument.All
+                .Where(e => e.TagName == "IMG" && e.HasAttribute("SRC"))
+                .Select(e => e.GetAttribute("SRC"))
+                .Where(e => e != null)
+                .Distinct();
+
+            return imageUrls;
+        }
+
+        internal static void CreatePath(string path)
+        {
+            Directory.CreateDirectory(path);
         }
     }
 }
--- ImageGrabTests/ImageGrabberTests.cs
+++ ImageGrabTests/ImageGrabberTests.cs
@@ -1,26 +1,124 @@
-﻿using ImageGrab;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using ImageGrab;
+using NUnit.Framework;
 
 namespace ImageGrabTests
 {
-    [TestClass]
+    [TestFixture]
     public class ImageGrabberTests
     {
-        [TestMethod]
+        [Test]
+        [TestCase("http://www.google.com/foo/bar", "http://www.google.com", new[] { "foo", "bar" })]
+        [TestCase("http://www.google.com/foo/bar/index.html", "http://www.google.com", new[] { "foo", "bar" })]
+        [TestCase("http://www.google.com/foo/bar?toodle=1325", "http://www.google.com", new[] { "foo", "bar" })]
+        [TestCase("http://www.google.com/foo/bar#app/stuff/biz?toodle=1325", "http://www.google.com", new[] { "foo", "bar" })]
+        public void BaseUrlTests(string baseUrl, string expectedHost, string[] expectedPath)
+        {
+            var uri = new Uri(baseUrl, UriKind.Absolute);
+            var host = uri.Host;
+            var scheme = uri.Scheme;
+            
+            var hostUrl = (!string.IsNullOrWhiteSpace(scheme) ? scheme + "://" : "") + host;
+            var localPath = uri.LocalPath.Split(new[] {'/'}, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.AreEqual(expectedHost, hostUrl);
+            CollectionAssert.AreEqual(expectedPath, localPath);
+        }
+
+        [Test]
+        public void GetHtmlTest()
+        {
+            var imageGrabber = new ImageGrabber();
+            var html = imageGrabber.GetHtml("http://www.google.com");
+
+            Assert.IsNotNull(html);
+        }
+
+
+        [Test]
+        public void GetImageUrlsFromHtmlTest()
+        {
+            var testHtml = @"
+
+<!DOCTYPE html>
+<html lang=""en"">
+<head>
+ 	<meta http-equiv=""Content-type"" content=""text/html; charset=utf-8"" />
+	<link rel=""stylesheet"" type=""text/css"" href=""/css/style.css"">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>    
+   <title>Scott Hanselman - Coder, Blogger, Teacher, Speaker, Author</title>
+</head>
+<body class=""line-darkbrown"">
+<section class=""containerOuter line-tan"" id=""topbar"">
+    <section class=""containerInner"">
+        <h1><a href=""/"">Scott Hanselman</a></h1>
+        <nav>
+            <ul>
+                <li><a href=""http://hanselman.com/about"">about</a></li>
+                <li><a href=""http://hanselman.com/blog"" >blog</a></li>
+                <li><a href=""http://hanselman.com/speaking"">speaking</a></li>
+                <li><a href=""http://hanselman.com/podcasts"">podcasts</a></li>
+                <li><a href=""http://hanselman.com/books"">books</a></li>  
+          </ul>
+            <ul class=""itemFeed"">
+                  <li><a href=""http://www.hanselminutes.com""><img src=""/images/blog-hanselminutes.png"" alt=""The Hanselminutes Podcast"" /></a></li>
+                  <li><a href=""http://thisdeveloperslife.com""><img src=""/images/blog-tdl.png"" alt=""This Developer's Life"" /></a></li>
+                  <li><a href=""http://www.ratchetandthegeek.com""><img src=""/images/blog-rachetgeek.png"" alt=""Rachet and the Geek"" /></a></li>
+                  <li><a href=""http://friday.azure.com""><img src=""/images/blog-AzureFriday.png"" alt=""Azure Friday"" /></a></li>
+        </ul>              
+        </nav>        
+    </section>
+</section>
+</body>
+</html>
+";
+            var urls = ImageGrabber.GetImageUrlsFromHtml(testHtml);
+
+            var expectedUrls = new[]
+            {
+                "/images/blog-hanselminutes.png",
+                "/images/blog-tdl.png",
+                "/images/blog-rachetgeek.png",
+                "/images/blog-AzureFriday.png"
+            };
+
+            CollectionAssert.AreEquivalent(expectedUrls, urls);
+        }
+
+        [Test]
         public void SimpleTest()
         {
             var imageGrabber = new ImageGrabber();
             imageGrabber.GetImages("www.google.com", "");
         }
 
-        [TestMethod]
-        public void GetHtmlTest()
-        {
-            var imageGrabber = new ImageGrabber();
-            var html = imageGrabber.GetHtml("www.google.com");
 
-            Assert.IsNotNull(html);
+        [Test]
+        [TestCase("www.google.com", ExpectedResult = false)]
+        [TestCase("http://www.google.com", ExpectedResult = true)]
+        [TestCase("https://www.google.com", ExpectedResult = true)]
+        [TestCase("https://httpbin.org/get", ExpectedResult = true)]
+        public bool TestUrl(string url)
+        {
+            return ImageGrabber.CheckUrl(url);
         }
-        
+
+        [Test]
+        [TestCase(@"C:\temp\foo")]
+        [TestCase(@"C:\temp\foo\bz\doodle")]
+        [TestCase(@"C:\temp\foo.bz")]
+        [TestCase(@"C:\temp\foo/bz")]
+        public void VerifyFolderPathTest(string path)
+        {
+            try
+            {
+                ImageGrabber.CreatePath(path);
+            }
+            finally
+            {
+                Directory.Delete(path, true);
+            }
+        }
     }
 }
